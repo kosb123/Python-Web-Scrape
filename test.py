6c12b2db02b32b7aea7f7111f2cd0253616cb015
kosb123 <yjy980425@gmail.com>
2020-12-22 00:06:09 +0900

    #7_assignment

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-from babel.numbers import format_currency
+import requests
+from bs4 import BeautifulSoup
 
-print(format_currency(5000, "KRW", locale="ko_KR"))
+url = 'http://hochicken.alba.co.kr/job/brand/'
+result = requests.get(url)
+soup = BeautifulSoup(result.text, "html.parser")
+jobs = soup.find("tbody").find_all("tr")
+# for job in jobs:
+job = jobs[0]
+print(job['id'])
